21b46f07949d4127916868a2d19d0cf0f7b8ef52
7mek7 <129232980+7mek7@users.noreply.github.com>
2024-09-14 08:21:55 +0330

    Update and rename example4(blue print) to (blue print).py

---
+++ flask1/examples/(blue print).py
@@ -0,0 +1,16 @@
+use bluprint:
+
+from flask import Flask, Blueprint
+
+app = Flask(__name__)
+simple_page = Blueprint('simple_page', name)
+
+@simple_page.route('/simple')
+def simple():
+    return "اits a simple page."
+
+app.register_blueprint(simple_page)
+
+if name == '__main__':
+    app.run(debug=True)
+____________________________________________________________________________
